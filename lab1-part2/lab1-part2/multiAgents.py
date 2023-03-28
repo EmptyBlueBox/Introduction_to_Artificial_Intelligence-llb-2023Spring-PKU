69c25b7d24c382cc0c3ce887efc2a397e2ca8e27
EmptyBlueBox <lyt0112@stu.pku.edu.cn>
2023-03-29 04:02:32 +0800

    Update multiAgents.py

--- lab1-part2/lab1-part2/multiAgents.py
+++ lab1-part2/lab1-part2/multiAgents.py
@@ -247,38 +247,153 @@ class MCTSAgent(MultiAgentSearchAgent):
             '''
 
             def __init__(self, data):
-                self.north = None
-                self.east = None
-                self.west = None
-                self.south = None
-                self.stop = None
+                self.child_node = [None]*5  # 5个动作,N,E,W,S,Stop
+                # self.north = None
+                # self.east = None
+                # self.west = None
+                # self.south = None
+                # self.stop = None
                 self.parent = None
+                self.is_leaf = True  # 相对于已扩展的Monte Carlo Tree来说是否是叶子节点，新扩展的节点都是叶子节点
                 self.game_state = data[0]
                 self.numerator = data[1]
                 self.denominator = data[2]
 
-        data = [gameState, 0, 1]
-        mct_root = Node(data)
-        epochs = 1000
+        data = [gameState, 0, 1]  # data is the data of root node
+        mct_root = Node(data)  # mct_root is the root of MCTS tree
+        # AGENT_NUM is the number of agents in the game
+        AGENT_NUM = gameState.getNumAgents()
+        C = sqrt(2)  # C is the exploration constant
+        EPOCHS = 2500  # 模拟次数 2000
+        ITERATION = 15  # 每次模拟的最大深度 20
+        THRESHOLD = 10  # 模拟的阈值 10
+        NEAREST_K = 1  # 最近的k个节点 1
+        index2action = ['North', 'East', 'West', 'South', 'Stop']
+        action2index = {'North': 0, 'East': 1,
+                        'West': 2, 'South': 3, 'Stop': 4}
 
-        def Selection(root):
-            "*** YOUR CODE HERE ***"
+        def Selection(node, agent_index):
+            if node.game_state.isWin():  # 如果当前节点是胜利节点，直接返回1
+                return 1, node, agent_index
+            elif node.game_state.isLose():  # 如果当前节点是失败节点，直接返回0
+                return 0, node, agent_index
+            elif node.is_leaf:  # 如果当前节点是叶子节点，直接返回未定的输赢节点
+                return -1, node, agent_index
 
-        def Expansion(cgs, cgstree):
-            "*** YOUR CODE HERE ***"
-            util.raiseNotDefined()
+            nxt_agent_index = (agent_index + 1) % AGENT_NUM  # 下一个agent的index
+            ucb_val = -float('inf')  # 最大的ucb值
+            ln_t = log(node.denominator)  # 当前节点的模拟次数的对数
+            nxt_node = None  # 最大的ucb值对应的节点
+            # 如果是pacman就有5个动作（可以Stop），否则就有4个动作
+            for child_index in range(5 if agent_index == 0 else 4):
+                if node.child_node[child_index] is not None:  # 如果子节点已经expand过
+                    # 如果子节点已经expand但是未simulate，防止计算ucb时除以0
+                    if node.child_node[child_index].denominator == 0:
+                        return 0, node.child_node[child_index], nxt_agent_index
+                    # 如果子节点已经expand并且已经simulate，那么选择ucb最大的节点
+                    else:
+                        q = node.child_node[child_index].numerator / \
+                            node.child_node[child_index].denominator  # q是子节点的胜率
+                        # n是子节点的模拟次数
+                        n = node.child_node[child_index].denominator
+                        tmp_ucb_val = q+C*sqrt(ln_t/n)  # 这个子节点的ucb值
+                        if tmp_ucb_val > ucb_val:
+                            ucb_val = tmp_ucb_val
+                            nxt_node = node.child_node[child_index]
+            return Selection(nxt_node, nxt_agent_index)
 
-        def Simulation(cgs, cgstree):
-            "*** YOUR CODE HERE ***"
-            util.raiseNotDefined()
+        def Expansion(node, agent_index):
+            actions = node.game_state.getLegalActions(
+                agent_index)  # 获取当前节点的所有合法动作
+            if len(actions) == 0:  # 如果没有合法动作，返回False
+                return False, node, agent_index
+            else:
+                node.is_leaf = False  # 如果有合法动作，那么当前节点不是叶子节点
 
-        def Backpropagation(cgstree, WinorLose):
-            "*** YOUR CODE HERE ***"
-            util.raiseNotDefined()
+            can_simulate = []  # 可以模拟的动作
+            for action in actions:
+                # 生成子节点
+                child_node = Node(
+                    [node.game_state.generateSuccessor(agent_index, action), 0, 0])
+                child_node.parent = node
+                # 将子节点加入当前节点的子节点列表
+                node.child_node[action2index[action]] = child_node
+                can_simulate.append(child_node)  # 将子节点加入可以模拟的列表
 
-        def HeuristicFunction(currentGameState):
-            "*** YOUR CODE HERE ***"
-            util.raiseNotDefined()
+            nxt_agent_index = (agent_index + 1) % AGENT_NUM  # 下一个agent的index
+            # 随机选择一个可以模拟的动作，返回1
+            return True, random.choice(can_simulate), nxt_agent_index
 
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        def Simulation(node, agent_index):
+            state = node.game_state
+            index = agent_index
+            for iteration in range(ITERATION):
+                if state.isWin():
+                    return 1  # 这个节点最后得到了确定的结果且是胜利节点
+                elif state.isLose():
+                    return 0  # 这个节点最后得到了确定的结果且是失败节点
+                actions = state.getLegalActions(index)
+                if len(actions) == 0:  # 如果没有合法动作，返回失败
+                    return 0
+                else:  # 随机选择一个合法动作，这里可以进行启发式选择进行优化
+                    state = state.generateSuccessor(
+                        index, random.choice(actions))
+                    index = (index + 1) % AGENT_NUM
+            # 这个节点最后没有得到确定的结果，返回启发式函数的结果，函数越大越认为是胜利节点
+            return HeuristicFunction(state) >= THRESHOLD
+
+        def Backpropagation(node, is_win):
+            while node is not None:
+                node.numerator += is_win
+                node.denominator += 1
+                node = node.parent
+
+        def HeuristicFunction(game_state):
+            current_score = game_state.getScore()
+            pacman_pos = game_state.getPacmanPosition()
+            ghost_pos = game_state.getGhostPositions()
+            food_pos = game_state.getFood().asList()
+            min_pacman_ghost_dis = min([manhattanDistance(pacman_pos, ghost_pos[i]) for i in range(
+                len(ghost_pos))])  # pacman到ghost的最小距离
+            # if min_pacman_ghost_dis == 0:
+            #     return float('-inf')
+
+            sum_pacman_food_dis = 0
+            CNT = 0
+            while len(food_pos) > 0:  # 贪心计算pacman到food的总距离
+                min_index = None
+                min_dis = float('inf')
+                for i in range(len(food_pos)):  # 找到距离pacman最近的food
+                    tmp_dis = manhattanDistance(
+                        pacman_pos, food_pos[i])
+                    if tmp_dis < min_dis:
+                        min_dis = tmp_dis
+                        min_index = i
+                sum_pacman_food_dis += min_dis  # 将最近的food的距离加入总距离
+                pacman_pos = food_pos[min_index]  # 更新pacman的位置
+                del food_pos[min_index]  # 删除已经计算过的food
+                if CNT == NEAREST_K:
+                    break
+            return current_score+min_pacman_ghost_dis-sum_pacman_food_dis
+
+        for epoch in range(EPOCHS):  # 进行EPOCHS次迭代
+            win_or_lose, leaf, leaf_agent_index = Selection(mct_root, 0)  # 选择
+            if win_or_lose == -1:  # 不知道叶子节点的胜负
+                expand_vaild, leaf, leaf_succ_agent_index = Expansion(
+                    leaf, leaf_agent_index)  # 扩展来生成新的叶子节点
+                if expand_vaild:
+                    win_or_lose = Simulation(leaf, leaf_succ_agent_index)
+                else:
+                    win_or_lose = 0
+            Backpropagation(leaf, win_or_lose)
+
+        ans = None
+        max_Q = -float('inf')
+        for i in range(5):
+            if mct_root.child_node[i] is not None:
+                tmp_Q = mct_root.child_node[i].numerator / \
+                    mct_root.child_node[i].denominator
+                if tmp_Q > max_Q:
+                    max_Q = tmp_Q
+                    ans = i
+        return index2action[ans]
